6b644c7b128b716b61f3de292030058f508e4ce7
shinemoon <shinemoon@foxmail.com>
2023-08-11 21:12:49 +0800

    Dump and framework OK

--- dumpBook.py
+++ dumpBook.py
@@ -1,5 +1,6 @@
+import subprocess
 import requests
-import pprint
+from pprint import pprint as ppt
 import html
 from pyquery import PyQuery as pq
 from fake_useragent import UserAgent
@@ -12,6 +13,15 @@ from time import sleep
 from tqdm import tqdm,trange
 
 from termcolor import colored, cprint
+import hashlib
+import random
+
+
+import argparse
+
+# Debug Flag
+debugFlag = True
+debugSample = 10
 
 # Config Relavant
 siteConfigs = {
@@ -30,50 +40,101 @@ headers = {
     "User-Agent": random_user_agent
 }
 
-bookId = '/5/5395/'
-indexPage = siteConfigs['url']+bookId
-
 def getSingle(url):
+    fId = hashlib.sha1(url.encode("UTF-8")).hexdigest()[:10];
     results = requests.get(url, headers=headers)
     results.encoding='utf-8'
     if(results.status_code==200):
         doc = pq(results.text)
         for k in siteConfigs['excludeKeys']:
             doc(k).remove()
-        # Title
-        cprint(doc(siteConfigs['titleKey']).text(),'light_grey',attrs=['dark'])
-        # Content
-        print(doc(siteConfigs['contentKey']).text())
+        dumpContent = {
+            # Title
+            'title':doc(siteConfigs['titleKey']).text(),
+            # Content
+            'content':(doc(siteConfigs['contentKey']).text()),
+            # url
+            'url':url,
+        }
+
+        #pdb.set_trace()
+        #ppt(dumpContent)
+        with open(r'working/'+wId+'/'+fId, 'w', encoding='utf8') as fp:
+            json.dump(dumpContent,fp,ensure_ascii = False)
+        return 0
     else:
-        cprint('读取'+url+'失败','light_red',attrs=['dark'])
-    return 0
+        return -1
 
 
 
-def fetchContent(start):
-    cprint("开始获取正文,从第"+str(int(start+1))+"篇开始",'light_blue',attrs=['bold'])
+def fetchContent(resume):
+    # Logged list
+    hlist = []
+    # Index List
     ilist = []
-    # Create table cache
+
+    # Gap List
+    glist = []
     try:
-        with open(r'workingList', 'r', encoding='utf8') as fp:
-            cprint("读取列表完成",'blue',attrs=['dark'])
+        with open(r'working/'+wId+'/workingList', 'r', encoding='utf8') as fp:
+            cprint("读取目录列表完成",'blue',attrs=['dark'])
             ilist = json.load(fp)
-            #pprint.pprint(ilist)
     except:
             cprint("工作列表读取失败",'red',attrs=['bold'])
             return -1
-    #with trange(len(ilist)) as t:
-    with trange(3) as t:
+
+    if(resume):
+        cprint("继续以前历史下载",'blue',attrs=['dark'])
+        # Need to sort out worklist corss compare the workingList vs. updatedList
+        try:
+            with open(r'working/'+wId+'/updatedList', 'r', encoding='utf8') as fp:
+                cprint("读取历史列表完成",'blue',attrs=['dark'])
+                hlist = json.load(fp)
+        except:
+                cprint("工作列表读取失败",'red',attrs=['bold'])
+                return -1
+        glist = [item for item in ilist if item not in hlist]
+    else:
+        subprocess.run("rm -f working/"+wId+"/updatedList", shell=True, check=True)
+        glist = ilist
+        cprint("重新开始下载所有",'blue',attrs=['dark'])
+
+    cprint("开始获取正文",'light_blue',attrs=['bold'])
+    for i in glist:
+        print(i['title'])
+
+    listSize = len(glist)
+    errCount = 0
+
+    with trange(listSize) as t:
+        t.set_description(colored('开始获取','light_cyan',attrs=['dark']))
         for i in t:    
-            t.set_description(colored('正在获取第 %i 篇' % i,"light_cyan"))
-            sleep(3)
-            getSingle(siteConfigs['url']+ilist[i]['url'])
-            sleep(0.01)
-        cprint("获取完成,共"+str(int(i+1))+"篇",'light_blue',attrs=['bold'])
+            res = 0
+            if(debugFlag):
+                #res = random.choice([0,-1])
+                res = getSingle(siteConfigs['url']+glist[i]['url'])
+                sleep(5)
+            else:
+                res = getSingle(siteConfigs['url']+glist[i]['url'])
+            sleep(0.1)
+            if(res!=-1):
+                hlist.append(glist[i])
+                t.set_description(colored('获取第 %i 篇. 存入文件'%(i+1),'light_cyan',attrs=['dark']))
+            else:
+                errCount=errCount+1
+                t.set_description(colored("提取第%i篇失败"%i,'red',attrs=['bold']));
+            try:
+                with open(r'working/'+wId+'/updatedList', 'w', encoding='utf8') as fp:
+                    json.dump(hlist,fp,ensure_ascii = False)
+            except:
+                cprint("工作列表刷新失败",'light_yellow',attrs=['dark'])
+        cprint("获取完成,共"+str(listSize)+"篇，其中失败"+str(errCount)+"篇。",'light_blue',attrs=['bold'])
     return 0 
 
 def parseIndex(url):
     cprint("开始刷新目录",'light_blue',attrs=['bold'])
+    cprint("创建工作目录",'blue',attrs=['dark'])
+    subprocess.run("mkdir -p working/"+wId, shell=True, check=True)
     ilist=[]
     results = requests.get(indexPage, headers=headers)
     results.encoding='utf-8'
@@ -85,29 +146,57 @@ def parseIndex(url):
         # pyQuery
         doc = pq(results.text)
         blist = doc(siteConfigs['indexKey'])
-        for b in range(len(blist)):
+        ilen = len(blist)
+        if(debugFlag):
+            ilen = debugSample
+        for b in range(ilen):
             ilist.append({
                 'title':blist.eq(b).text(),
                 'url':blist.eq(b).attr('href'),
+                'fId':hashlib.sha1((siteConfigs['url']+blist.eq(b).attr('href')).encode("UTF-8")).hexdigest()[:10],
             })
-
         # Create table cache
         try:
-            with open(r'workingList', 'w', encoding='utf8') as fp:
+            with open(r'working/'+wId+'/workingList', 'w', encoding='utf8') as fp:
                 json.dump(ilist,fp,ensure_ascii = False)
-            print("WorkingList Create Done.")
         except:
-            print("WorkingList Create Failed.")
+            cprint("工作列表创建失败列表完成",'red',attrs=['dark'])
+            exit(1)
         return 0 
     else:
         print("Failed to get valid page!")
         return -1
 
-        #pdb.set_trace()
 
 # Refresh Working List
-#parseIndex(indexPage)
 
 # Start/Resume to fetch content
-fetchContent(0)
+
+if __name__=='__main__':
+    global bookId, indexPage, wId
+    # Initialization of parser
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("bookId", help="目录页ID")
+    parser.add_argument("-n","--newindex", help="重新刷新目录", action="store_true")
+    parser.add_argument("-r","--resume", help="继续未完成的任务", action="store_true")
+    
+    args = parser.parse_args()
+
+    #bookId = '/5/5395/'
+    bookId = args.bookId
+    indexPage = siteConfigs['url']+bookId
+    wId = hashlib.sha1(indexPage.encode("UTF-8")).hexdigest()[:10];
+
+
+    # 目录处理
+    if(args.newindex):
+        cprint("重新刷新目录",'blue',attrs=['dark'])
+
+
+        parseIndex(indexPage)
+    else:
+        cprint("不刷新目录",'blue',attrs=['dark'])
+    
+    fetchContent(args.resume)
 
